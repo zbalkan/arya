731c0ca3c50223311f3c554fdfd4776211d3b3dc
Zafer Balkan <zafer@zaferbalkan.com>
2022-07-03 21:02:44 +0300

    Used static typing where possible in file_mapper.py

--- src/file_mapper.py
+++ src/file_mapper.py
@@ -1,10 +1,55 @@
 import random
+from tkinter.messagebox import NO
 
 from consts import CONFICKER_FIRST_4KB
 
 PE_HEADER_OFFSET = 2048
 
-def _add_func_pre_and_epi(func):
+class FileMapper:
+    def __init__(self, add_pe_header : bool, malware_file : str) -> None:
+        self._byte_mapping : list = []
+        self._malware_bytes = self._read_malware(malware_file)
+        if add_pe_header:
+            self._get_pe_header()
+
+    def is_slice_empty(self, start_index : int, end_index : int) -> bool:
+        if len(self._byte_mapping) < end_index:
+            self._byte_mapping += [None] * (end_index - len(self._byte_mapping))
+
+        return all([True if byte is None else False for byte in self._byte_mapping[start_index:end_index]])
+
+    def is_slice_reserved(self, start_index : int, end_index : int) -> bool:
+        return all([True if place == "reserved" else False for place in self._byte_mapping[start_index:end_index]])
+
+    def place(self, byte_string : str, start_index : int, pre_reserved : bool =False) -> None:
+        end_index = start_index + len(byte_string)
+        if self.is_slice_empty(start_index, end_index) or (pre_reserved and self.is_slice_reserved(start_index, end_index)):
+            self._byte_mapping = self._byte_mapping[:start_index] + list(byte_string) + self._byte_mapping[end_index:]
+
+    def append(self, byte_string : bytes) -> None:
+        self._byte_mapping.extend(byte_string)
+
+    def _get_first_free_spot(self, length : int) -> int:
+        for curr_index, value in enumerate(self._byte_mapping):
+            if self.is_slice_empty(curr_index, curr_index + length):
+                return curr_index
+        return len(self._byte_mapping)
+
+    def reserve_first_free_spot(self, length : int) -> int:
+        first_free_index = self._get_first_free_spot(length)
+        self._byte_mapping = self._byte_mapping[:first_free_index] + (["reserved"] * length) + self._byte_mapping[first_free_index + length:]
+        return first_free_index
+
+    def _get_pe_header(self) -> None:
+        self.append(self._malware_bytes[:PE_HEADER_OFFSET])
+
+    def _read_malware(self, file_name : str) -> bytes:
+        if file_name:
+            with open(file_name, "rb") as malware_file:
+                 return malware_file.read()
+        else:
+            return CONFICKER_FIRST_4KB
+
     # function Prologue Example
     # 55 89 e5 83 ec 0c
     # 0:  55                      push   ebp
@@ -16,71 +61,23 @@ def _add_func_pre_and_epi(func):
     # 0:  89 ec                   mov    esp,ebp
     # 2:  5d                      pop    ebp
     # 3:  c3                      ret
-    def inner(self, length):
-        if length > 10:
-            start = b"\x55\x89\xe5\x83\xec" + (4 * random.randint(1, 30)).to_bytes(1, byteorder="little", signed=True)
-            end = b"\x89\xec\x5d\xc3"
-            return start + func(self, length)[len(start):-1 * len(end)] + end
-        else:
-            return func(self, length)
-    return inner
-
-class FileMapper:
-    def __init__(self, add_pe_header, malware_file):
-        self._byte_mapping = []
-        self._malware_bytes = self._read_malware(malware_file)
-        if add_pe_header:
-            self._get_pe_header()
-
-    def is_slice_empty(self, start_index, end_index):
-        if len(self._byte_mapping) < end_index:
-            self._byte_mapping += [None] * (end_index - len(self._byte_mapping))
-
-        return all([True if byte is None else False for byte in self._byte_mapping[start_index:end_index]])
-
-    def is_slice_reserved(self, start_index, end_index):
-        return all([True if place == "reserved" else False for place in self._byte_mapping[start_index:end_index]])
-
-    def place(self, byte_string, start_index, pre_reserved=False):
-        end_index = start_index + len(byte_string)
-        if self.is_slice_empty(start_index, end_index) or (pre_reserved and self.is_slice_reserved(start_index, end_index)):
-            self._byte_mapping = self._byte_mapping[:start_index] + list(byte_string) + self._byte_mapping[end_index:]
-
-    def append(self, byte_string):
-        self._byte_mapping.extend(byte_string)
-
-    def _get_first_free_spot(self, length):
-        for curr_index, value in enumerate(self._byte_mapping):
-            if self.is_slice_empty(curr_index, curr_index + length):
-                return curr_index
-        return len(self._byte_mapping)
-
-    def reserve_first_free_spot(self, length):
-        first_free_index = self._get_first_free_spot(length)
-        self._byte_mapping = self._byte_mapping[:first_free_index] + (["reserved"] * length) + self._byte_mapping[first_free_index + length:]
-        return first_free_index
-
-    def _get_pe_header(self):
-        self.append(self._malware_bytes[:PE_HEADER_OFFSET])
-
-    def _read_malware(self, file_name):
-        if file_name:
-            with open(file_name, "rb") as malware_file:
-                 return malware_file.read()
-        else:
-            return CONFICKER_FIRST_4KB
-
-    @_add_func_pre_and_epi
-    def generate_random_x86_code(self, length):
+    def generate_random_x86_code(self, length : int) -> str:
         if self.get_malware_len() - PE_HEADER_OFFSET < length:
             start_index = random.randint(PE_HEADER_OFFSET, self.get_malware_len() - 1)
             mult = length // (self.get_malware_len() - start_index) + 1
-            return (self._malware_bytes[start_index:self.get_malware_len()] * mult)[0:length]
+            malware_bytes = (self._malware_bytes[start_index:self.get_malware_len()] * mult)[0:length]
         else:
             start_index = random.randint(PE_HEADER_OFFSET, self.get_malware_len() - length)
-            return self._malware_bytes[start_index:start_index + length]
+            malware_bytes = self._malware_bytes[start_index:start_index + length]
 
-    def _get_none_mapping(self):
+        if length > 10:
+            start = b"\x55\x89\xe5\x83\xec" + (4 * random.randint(1, 30)).to_bytes(1, byteorder="little", signed=True)
+            end = b"\x89\xec\x5d\xc3"
+            return (start + malware_bytes[len(start):-1 * len(end)] + end).decode("utf-8")
+        else:
+            return  malware_bytes.decode("utf-8")
+
+    def _get_none_mapping(self) -> list:
         is_prev_none = False
         curr_start = 0
         start_end_mapping = []
@@ -95,17 +92,17 @@ class FileMapper:
 
         return start_end_mapping
 
-    def fill_empty_with_code(self):
+    def fill_empty_with_code(self) -> None:
         none_mapping = self._get_none_mapping()
 
         for start_index, end_index in none_mapping:
             self.place(self.generate_random_x86_code(end_index - start_index), start_index)
 
-    def get_as_bytestream(self):
+    def get_as_bytestream(self) -> bytes:
         return bytes(self._byte_mapping)
 
-    def get_file_len(self):
+    def get_file_len(self) -> int:
         return len(self._byte_mapping)
 
-    def get_malware_len(self):
+    def get_malware_len(self) -> int:
         return len(self._malware_bytes)
